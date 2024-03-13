f27526261825d65ee759e5308e2d8dbc5534cbda
YoungKid <131250599+YoungKid228@users.noreply.github.com>
2024-03-13 15:16:20 +1000

    Add files via upload

---
+++ larkin.rb
@@ -0,0 +1,9 @@
+=begin
+
+                            Online Ruby Compiler.
+                Code, Compile, Run and Debug Ruby script online.
+Write your code in this editor and press "Run" button to execute it.
+
+=end
+
+puts "Hello World"
